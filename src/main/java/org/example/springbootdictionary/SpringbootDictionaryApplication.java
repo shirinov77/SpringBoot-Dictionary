b4b6d3a55ef6e6f883cb04c179181377bdf7a965
Muhammad <muhammadshirinov11@gmail.com>
2025-08-09 11:08:56 +0500

    create new simple project

---
+++ src/main/java/org/example/springbootdictionary/SpringbootDictionaryApplication.java
@@ -0,0 +1,13 @@
+package org.example.springbootdictionary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDictionaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDictionaryApplication.class, args);
+    }
+
+}
